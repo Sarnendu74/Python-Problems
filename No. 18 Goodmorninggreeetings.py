d917db6d993118972e936adceafbc609485b5032
Sarnendu Ghosh <154461219+Sarnendu74@users.noreply.github.com>
2024-03-27 11:58:25 +0530

    No. 18 Goodmorninggreeetings.py

--- No. 18 Goodmorninggreeetings.py
+++ No. 18 Goodmorninggreeetings.py
@@ -1,10 +1,11 @@
 #Good Morning
 import time
+name=input("Enter Your Name: ")
 hour = int(time.strftime('%H'))
 print(hour)
 if hour>0 and hour<12 :
-    print("Good Morning Sarnendu")
+    print("Good Morning", name)
 elif hour<12 and hour>5:
-    print("Good Evening Sarnendu")
+    print("Good Evening", name)
 else:
-    print("Good Night Sarnendu")
+    print("Good Night", name)
