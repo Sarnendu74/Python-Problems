908d81fccaef3635447a2f0f617ea75166124598
Sarnendu Ghosh <154461219+Sarnendu74@users.noreply.github.com>
2024-07-09 14:28:57 +0530

    No. 25 : swapping first 2 letter and space separated .py

---
+++ No. 25 : swapping first 2 letter and space separated.py
@@ -0,0 +1,12 @@
+#first string input
+str1 = "python"
+#second string input
+str2 = "programming"
+#swapping
+swap1 = str2[:2] + str1[2:]
+swap2 = str1[:2] + str2[2:]
+#output
+print("Space separated and swap string is :", swap1 + ' ' + swap2)
+
+#output is:
+Space separated and swap string is : prthon pyogramming
