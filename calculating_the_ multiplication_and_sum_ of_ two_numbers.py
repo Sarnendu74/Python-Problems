4ce5e1f8ecc1e290f37798b533f4a25cefe43acd
Sarnendu Ghosh <154461219+Sarnendu74@users.noreply.github.com>
2024-08-03 00:32:54 +0530

    calculating_the_ multiplication_and_sum_ of_ two_numbers

---
+++ calculating_the_ multiplication_and_sum_ of_ two_numbers.py
@@ -0,0 +1,8 @@
+# Declare two variables
+x = 5
+y = 10
+# Calculating multiplication and sum
+multiplication = x*y
+sum = x+y
+# displaying 
+print(x,"+",y,"=",sum,"\nAnd",x,"*",y,"=",multiplication)
