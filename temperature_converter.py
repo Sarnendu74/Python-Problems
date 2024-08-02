33932c045821bcfc2e78317e590c2127b4268bba
Sarnendu Ghosh <154461219+Sarnendu74@users.noreply.github.com>
2024-08-03 00:32:13 +0530

    No. 29 temperature_converter.py

---
+++ temperature_converter.py
@@ -0,0 +1,11 @@
+# Centigrade to Fahrenheit Converter display for styling
+print("Centigrade to Fahrenheit Converter by Sarnendu")
+
+# Declare Centigrade variable
+centigrade = 100
+
+# Convert Centigrade to Fahrenheit
+fahrenheit = (centigrade * 9/5) + 32
+
+# Print the result
+print(centigrade,"°C is equal to",fahrenheit,"°F")
