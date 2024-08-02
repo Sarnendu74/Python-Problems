57ab17c6cd21bc5b929a10f52e87ce97259f15e0
Sarnendu Ghosh <154461219+Sarnendu74@users.noreply.github.com>
2024-08-03 00:33:38 +0530

    largest_number_using_ternary_ operators

---
+++ ternary_ operators.py
@@ -0,0 +1,7 @@
+#declare two variables
+num1 = 75
+num2 = 99
+#using ternary operator for checking largest
+largest = num1 if num1 > num2 else num2
+# displaying largest one
+print("Largest number is:",largest)
