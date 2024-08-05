4265f3a688c76e03b4370ff20a1bc3ca612552dc
Sarnendu Ghosh <154461219+Sarnendu74@users.noreply.github.com>
2024-08-05 13:59:00 +0530

    Simple Interest

---
+++ calculate_si.py
@@ -0,0 +1,11 @@
+# Taking input from user
+principal = float(input("Enter your principal: "))
+rate = float(input("Enter your rate of principal: "))
+time = float(input("Enter your time: "))
+
+# Calculating Simple Interest
+simple_interest = (principal*rate*time) / 100
+
+# Displaying SI
+print("Simple Interest is: ", simple_interest)
+
