7a895c7d90f5672c1adf347c9373a255e06c25bf
Sarnendu Ghosh <154461219+Sarnendu74@users.noreply.github.com>
2024-08-05 14:00:33 +0530

    Converting_km_to_miles

---
+++ convert_km_to_miles.py
@@ -0,0 +1,6 @@
+# Takng input from user
+km = float(input("Enter the distance in kilometers: "))
+# Converting miles
+miles = km * 0.621371
+# Displaying ...
+print(f"{km} kilometers is equal to {miles} miles.")
